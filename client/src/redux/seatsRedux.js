e3ad151daee24bae402952261186ad8a8bb46223
Piotr Węgłowski <weglowskipiotr@gmail.com>
2024-01-30 00:11:36 +0100

    Prepare to publish

--- client/src/redux/seatsRedux.js
+++ client/src/redux/seatsRedux.js
@@ -32,7 +32,6 @@ export const loadSeatsRequest = () => {
     dispatch(startRequest({ name: 'LOAD_SEATS' }));
     try {
       let res = await axios.get(`${API_URL}/seats`);
-      await new Promise((resolve) => setTimeout(resolve, 1000));
       dispatch(loadSeats(res.data));
       dispatch(endRequest({ name: 'LOAD_SEATS' }));
     } catch (e) {
@@ -46,7 +45,6 @@ export const addSeatRequest = (seat) => {
     dispatch(startRequest({ name: 'ADD_SEAT' }));
     try {
       let res = await axios.post(`${API_URL}/seats`, seat);
-      await new Promise((resolve) => setTimeout(resolve, 1000));
       dispatch(addSeat(res));
       dispatch(endRequest({ name: 'ADD_SEAT' }));
     } catch (e) {
